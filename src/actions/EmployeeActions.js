9a789ecbe893f227b7be4758ec0d648b4d0a6cbb
farooqmajeed <farooq306@hotmail.com>
2017-07-12 12:52:55 +0500

    actions

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -3,7 +3,8 @@ import { Actions } from 'react-native-router-flux';
 
 import {
     EMPLOYEE_UPDATE,
-    EMPLOYEE_CREATE
+    EMPLOYEE_CREATE,
+    EMPLOYEES_FETCH_SUCCESS
 } from './types'
 
 export const employeeUpdate = ({ prop, value}) => {
@@ -22,6 +23,17 @@ export const employeeCreate = ({ name, phone, shift }) => {
             .then(() => {
                 dispatch({ type: EMPLOYEE_CREATE });
                 Actions.employeeList({ type: 'reset' })
+            });
+    };
+};
+
+export const employeesFetch = () =>{
+    const { currentUser } = firebase.auth();
+
+    return (dispatch) =>{
+        firebase.database().ref(`/users/${currentUser.uid}/employees`)
+            .on('value', snapshot => { 
+                dispatch({ type: EMPLOYEES_FETCH_SUCCESS, payload:snapshot.val() });
             });
     };
 };
--- src/actions/types.js
+++ src/actions/types.js
@@ -4,6 +4,7 @@ export const LOGIN_USER_SUCCESS = 'login_user_success';
 export const LOGIN_USER_FAIL = 'login_user_fail';
 export const LOGIN_USER = 'login_user';
 
-export const EMPLOYEE_UPDATE = 'empolye_update';
-export const EMPLOYEE_CREATE = 'empolye_create';
+export const EMPLOYEE_UPDATE = 'empolyee_update';
+export const EMPLOYEE_CREATE = 'empolyee_create';
+export const EMPLOYEES_FETCH_SUCCESS = 'employee_fetch_success';
 
